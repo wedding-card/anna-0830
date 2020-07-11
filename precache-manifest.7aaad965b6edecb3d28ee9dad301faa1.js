9941a7f3fa1448123a635c06c3e43686999579b1
kihoonkim <koreakihoon@gmail.com>
2020-07-11 13:19:30 +0900

    Updates

---
+++ precache-manifest.7aaad965b6edecb3d28ee9dad301faa1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6256c7cfe25c67310aa50b6aeee3a3f1",
+    "url": "/anna-0830/index.html"
+  },
+  {
+    "revision": "2a836ff1ec9aff108bc5",
+    "url": "/anna-0830/static/css/2.d3d912f3.chunk.css"
+  },
+  {
+    "revision": "e30487a6f2517cea625f",
+    "url": "/anna-0830/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2a836ff1ec9aff108bc5",
+    "url": "/anna-0830/static/js/2.b69ec3b1.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "/anna-0830/static/js/2.b69ec3b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e30487a6f2517cea625f",
+    "url": "/anna-0830/static/js/main.10b4ed02.chunk.js"
+  },
+  {
+    "revision": "1503c53fad43a0e19869",
+    "url": "/anna-0830/static/js/runtime-main.744d336d.js"
+  },
+  {
+    "revision": "0fbc36938fe3950db3ec1296eb4deba5",
+    "url": "/anna-0830/static/media/1.0fbc3693.jpg"
+  },
+  {
+    "revision": "bdf49c54bce62a6404a7e43d66b2339e",
+    "url": "/anna-0830/static/media/2.bdf49c54.jpg"
+  },
+  {
+    "revision": "ffb72fe20b2cd24640a7bb04e15a2367",
+    "url": "/anna-0830/static/media/4.ffb72fe2.jpg"
+  },
+  {
+    "revision": "23655a84b34dad67a83e24c1a8e339a0",
+    "url": "/anna-0830/static/media/5.23655a84.jpg"
+  },
+  {
+    "revision": "e3e328d1eff4cd9066f501204fae34e1",
+    "url": "/anna-0830/static/media/6.e3e328d1.jpg"
+  },
+  {
+    "revision": "baa0f7b401dc63e64795e24ea2a4571e",
+    "url": "/anna-0830/static/media/7.baa0f7b4.jpg"
+  },
+  {
+    "revision": "83298343816c0d97d481f5a4d8964ade",
+    "url": "/anna-0830/static/media/main.83298343.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.119eb8b474374edad7d06cb23dbea1d3.js"
+  "/anna-0830/precache-manifest.7aaad965b6edecb3d28ee9dad301faa1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/anna-0830/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
